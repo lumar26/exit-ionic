ad84a2c0701ae1e33af1ab72a984ef2a7b2b0b59
Luka Marinković <lukamarinkovic2602@gmail.com>
2022-05-03 15:26:04 +0200

    Implemented update and delete for performers

--- src/components/performer/admin/PerformerCardAdminControls.tsx
+++ src/components/performer/admin/PerformerCardAdminControls.tsx
@@ -1,40 +1,68 @@
-import React from "react";
-import {IonButton, IonCol, IonGrid, IonIcon, IonRow} from "@ionic/react";
+import React, {useState} from "react";
+import {IonButton, IonCol, IonGrid, IonIcon, IonModal, IonRow} from "@ionic/react";
 import {create, trash} from "ionicons/icons";
 import Performer from "../../../model/Performer";
+import {usePerformers} from "../../../store/PerformersContext";
+import UpdatePerformerForm from "./UpdatePerformerForm";
 
-const PerformerCardAdminControls : React.FC<{
+const PerformerCardAdminControls: React.FC<{
     performer: Performer
 }> = ({performer}) => {
-  return (
-      <IonGrid>
-          <IonRow>
-              <IonCol>
-                  <IonButton expand={"block"} color={'danger'}>
-                      <IonIcon
-                          icon={trash}
-                          className="tableIcon"
-                          size="medium"
-                      />
-                      Delete
-                  </IonButton>
-              </IonCol>
-          </IonRow>
-          <IonRow>
-              <IonCol>
-                  <IonButton expand={"block"}>
-                      <IonIcon
-                          icon={create}
-                          className="tableIcon"
-                          color="white"
-                          size="medium"
-                      />
-                      Update
-                  </IonButton>
-              </IonCol>
-          </IonRow>
-      </IonGrid>
-  );
+
+    const [showModalUpdate, setShowModalUpdate] = useState(false);
+
+
+    const performersContext = usePerformers();
+
+    const deletePerformer = () => {
+        performersContext.deletePerformer(performer);
+    };
+    return (
+        <IonGrid>
+            <IonRow>
+                <IonCol>
+                    <IonButton
+                        onClick={deletePerformer}
+                        expand={"block"} color={'danger'}>
+                        <IonIcon
+                            icon={trash}
+                            className="tableIcon"
+                            size="medium"
+                        />
+                        Delete
+                    </IonButton>
+                </IonCol>
+            </IonRow>
+            <IonRow>
+                <IonModal
+                    onIonModalDidDismiss={() => setShowModalUpdate(false)}
+                    isOpen={showModalUpdate}
+                >
+                    <UpdatePerformerForm performer={performer}/>
+                    <IonButton
+                        color="grey"
+                        size="default"
+                        onClick={() => setShowModalUpdate(false)}
+                    >
+                        Close
+                    </IonButton>
+                </IonModal>
+                <IonCol>
+                    <IonButton
+                        onClick={() => setShowModalUpdate(true)}
+                        expand={"block"}>
+                        <IonIcon
+                            icon={create}
+                            className="tableIcon"
+                            color="white"
+                            size="medium"
+                        />
+                        Update
+                    </IonButton>
+                </IonCol>
+            </IonRow>
+        </IonGrid>
+    );
 }
 
 export default PerformerCardAdminControls;
